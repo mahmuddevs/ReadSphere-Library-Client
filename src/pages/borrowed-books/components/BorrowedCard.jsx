5cee9309be5dce5dbfde0c7ad0f1eb458cec0d5e
Mahmud Rashid <srabon5704@gmail.com>
2024-12-26 15:24:52 +0600

    made some changes to the borrow book design

--- src/pages/borrowed-books/components/BorrowedCard.jsx
+++ src/pages/borrowed-books/components/BorrowedCard.jsx
@@ -15,8 +15,8 @@ const BorrowedCard = ({ _id, bookDetails, requestDate, returnDate, handleReturnB
             />
             <div className="p-4">
                 <h2 className="text-lg font-bold text-gray-800">{name}</h2>
-                <h4 className="text-base font-semibold text-gray-800">{author}</h4>
-                <p className="text-sm text-gray-600">Category: {category}</p>
+                <h4 className="text-base font-semibold text-gray-800"><span className="font-semibold text-gray-700">By:</span> {author}</h4>
+                <p className="text-sm font-medium text-gray-500"><span className="font-semibold text-gray-700">Category:</span> {category}</p>
                 <div className="flex items-center mt-2">
                     <ReactStars
                         count={5}
@@ -35,7 +35,7 @@ const BorrowedCard = ({ _id, bookDetails, requestDate, returnDate, handleReturnB
                     </p>
                 </div>
                 <div className="mt-4">
-                    <button onClick={() => { handleReturnBook(_id) }} className="btn btn-primary">Return Book</button>
+                    <button onClick={() => { handleReturnBook(_id) }} className="btn btn-sm btn-primary">Return Book</button>
                 </div>
             </div>
         </div>
